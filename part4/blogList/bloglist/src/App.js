7fe1a17afad3ea78925f58af6ad8b41cc488d9b2
Max Mynter <maxmynter@me.com>
2022-12-09 15:23:22 +0100

    Ex. 4.2

--- part4/blogList/bloglist/src/App.js
+++ part4/blogList/bloglist/src/App.js
@@ -1,22 +1,7 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-
-const baseURL = `http://localhost:${process.env.REACT_APP_PORT_BACKEND}/`;
-
-const Blogs = ({ blogEntries }) => {
-  return (
-    <>
-      {blogEntries.map((blog) => (
-        <div key={`${blog.title}-wrapper`}>
-          <div key={`${blog.title}-row-1`}>
-            {blog.title} by {blog.author} --- {blog.likes} Likes
-          </div>
-          <div key={`${blog.title}-row-2`}>{blog.url}</div>
-        </div>
-      ))}
-    </>
-  );
-};
+import Blogs from "./components/blogEntries";
+import baseURL from "./utils/config";
 
 const App = () => {
   const [blogEntries, setBlogEntries] = useState([]);
