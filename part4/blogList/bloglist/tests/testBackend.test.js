3f3d746b2421ed1c3d4c906faeec66cfbb91d8fc
Max Mynter <maxmynter@me.com>
2022-12-09 16:42:23 +0100

    Ex. 4.4-4.7

--- part4/blogList/bloglist/tests/testBackend.test.js
+++ part4/blogList/bloglist/tests/testBackend.test.js
@@ -1,8 +1,48 @@
 const listHelper = require("../backend/utils/list_helper");
 
-test("dummy returns one", () => {
-  const blogs = [];
+describe("Test list_helpers", () => {
+  const blogWithMostLikes = {
+    title: "M.J. is back",
+    author: "MJ",
+    url: "www.mj.de",
+    likes: 20,
+  };
+  const blogs = [
+    {
+      title: "WIll M.J dissapear",
+      author: "Not MJ",
+      url: "www.notMJ.de",
+      likes: 0,
+    },
+    { title: "M.J. is gone", author: "MJ", url: "www.mj.de", likes: 1 },
+    { title: "M.J. is away", author: "MJ", url: "www.mj.de", likes: 0 },
+    blogWithMostLikes,
+  ];
 
-  const result = listHelper.dummy(blogs);
-  expect(result).toBe(1);
+  test("dummy returns one", () => {
+    const result = listHelper.dummy(blogs);
+    expect(result).toBe(1);
+  });
+
+  test("totalLikes counts total likes", () => {
+    expect(listHelper.totalLikes(blogs)).toBe(21);
+  });
+
+  test("favouriteBlog returns blog with most likes", () => {
+    expect(listHelper.favouriteBlog(blogs)).toEqual(blogWithMostLikes);
+  });
+
+  test("mostBlogs returns the author of the most blogs and number of publications", () => {
+    expect(listHelper.mostBlogs(blogs)).toEqual({
+      author: "MJ",
+      blogs: 3,
+    });
+  });
+
+  test("mostLikes returns author and Likes of author with most likes", () => {
+    expect(listHelper.mostLikes(blogs)).toEqual({
+      author: "MJ",
+      likes: 21,
+    });
+  });
 });
