95a38e9429badb722b8d5857218f73452251b841
Max Mynter <maxmynter@me.com>
2022-12-28 02:25:32 +0100

    Modifications for Ex.5.x

--- part4/blogList/bloglist/backend/controllers/testing.js
+++ part4/blogList/bloglist/backend/controllers/testing.js
@@ -3,6 +3,7 @@ const jwt = require("jsonwebtoken");
 const testingRouter = require("express").Router();
 const Blog = require("../models/blogs");
 const User = require("../models/users");
+const middleware = require("../utils/middleware");
 
 testingRouter.post("/reset", async (request, response) => {
   await Blog.deleteMany({});
@@ -49,5 +50,28 @@ testingRouter.post("/login", async (request, response) => {
     .status(200)
     .send({ token, username: user.username, name: user.name, id: user._id });
 });
+
+testingRouter.post(
+  "/blog",
+  middleware.userExtractor,
+  async (request, response) => {
+    console.log("request.user", request.user);
+    try {
+      const loggedInUser = request.user;
+      if (loggedInUser) {
+        const blog = new Blog({ ...request.body, creator: loggedInUser });
+        const result = await blog.save();
+        return response.status(201).json(result);
+      } else {
+        return response.status(401).json({ error: "Authentication Error" });
+      }
+    } catch (error) {
+      if (error._message === "Blog validation failed") {
+        return response.status(400).json({ error: "Blog validation failed" });
+      }
+      return response.status(400).json({ error });
+    }
+  }
+);
 
 module.exports = testingRouter;
